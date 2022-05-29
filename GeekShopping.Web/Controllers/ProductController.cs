42d1b76c8d0c338df24587c39cc7acc39020011b
Brian Leonardo de Morais <morais.brian360@gmail.com>
2022-05-29 17:11:09 -0300

    Fim da seção 10

--- GeekShopping.Web/Controllers/ProductController.cs
+++ GeekShopping.Web/Controllers/ProductController.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
 using GeekShopping.Web.Services.Interfaces;
+using GeekShopping.Web.Models;
 
 namespace GeekShopping.Web.Controllers;
 
@@ -16,5 +17,64 @@ public class ProductController : Controller
     {
         var products = await _service.FindAll();
         return View(products);
+    }
+
+    public IActionResult ProductCreate()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> ProductCreate(ProductModel product)
+    {
+        if (ModelState.IsValid)
+        {
+            var response = await _service.Create(product);
+            if (response != null)
+                return RedirectToAction(nameof(Index));
+        }
+
+        return View(product);
+    }
+
+    public async Task<IActionResult> ProductUpdate(long id)
+    {
+        var product = await _service.FindById(id);
+        if (product != null)
+            return View(product);
+
+        return NotFound();
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> ProductUpdate(ProductModel product)
+    {
+        if (ModelState.IsValid)
+        {
+            var response = await _service.Update(product);
+            if (response != null)
+                return RedirectToAction(nameof(Index));
+        }
+
+        return View(product);
+    }
+
+    public async Task<IActionResult> ProductDelete(long id)
+    {
+        var product = await _service.FindById(id);
+        if (product != null)
+            return View(product);
+
+        return NotFound();
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> ProductDelete(ProductModel product)
+    {
+        var response = await _service.Delete(product.Id);
+        if (response)
+            return RedirectToAction(nameof(Index));
+        
+        return View(product);
     }
 }
