fe8ad596e253ee22ab0ef0b7d76eae08f98c71aa
Brian Leonardo de Morais <morais.brian360@gmail.com>
2022-06-11 17:58:21 -0300

    Ajuste em URL de imagem

--- GeekShopping.ProductAPI/Models/Context/MySqlContext.cs
+++ GeekShopping.ProductAPI/Models/Context/MySqlContext.cs
@@ -122,7 +122,7 @@ namespace GeekShopping.ProductAPI.Models.Context
                 Name = "Camiseta Goku Fases",
                 Price = new decimal(59.99),
                 Description = "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.<br/>The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English.<br/>Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy.",
-                ImageURL = "https://github.com/moraisbrian/GeekShoppingMicroservices/blob/master/ShoppingImages/13_dragon_ball.jpg",
+                ImageURL = "https://github.com/moraisbrian/GeekShoppingMicroservices/blob/master/ShoppingImages/13_dragon_ball.jpg?raw=true",
                 CategoryName = "T-shirt"
             });
         }
