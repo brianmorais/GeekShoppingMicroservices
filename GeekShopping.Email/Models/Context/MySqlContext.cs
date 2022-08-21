21f5d68c7667282add5196b48db77e55026afd0c
Brian Leonardo de Morais <morais.brian360@gmail.com>
2022-08-21 14:58:50 -0300

    Implementatdo serviço de envio de email

---
+++ GeekShopping.Email/Models/Context/MySqlContext.cs
@@ -0,0 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace GeekShopping.Email.Models.Context
+{
+    public class MySqlContext : DbContext
+    {
+        public MySqlContext(DbContextOptions<MySqlContext> options)
+            : base (options)
+        {
+        }
+
+        public DbSet<EmailLog> Emails { get; set; }
+    }
+}
--- GeekShopping.Email/Program.cs
+++ GeekShopping.Email/Program.cs
@@ -1,6 +1,22 @@
+using GeekShopping.Email.MessageConsumer;
+using GeekShopping.Email.Models.Context;
+using GeekShopping.Email.Repositories;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+var connection = builder.Configuration["MySQLConnection:MySQLConnectionString"];
+builder.Services.AddDbContext<MySqlContext>(options =>
+    options.UseMySql(connection, ServerVersion.AutoDetect(connection)));
+
+var dbContextBuilder = new DbContextOptionsBuilder<MySqlContext>();
+dbContextBuilder.UseMySql(connection, ServerVersion.AutoDetect(connection));
+
+builder.Services.AddSingleton(new EmailRepository(dbContextBuilder.Options));
+builder.Services.AddScoped<IEmailRepository, EmailRepository>();
+builder.Services.AddHostedService<RabbitMQPaymentConsumer>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
