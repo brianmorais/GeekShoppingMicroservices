7eaeb044f21cc6b6dd6b5bc114630d474bf5960b
Brian Leonardo de Morais <morais.brian360@gmail.com>
2022-09-09 20:52:19 -0300

    Configurações de rotas do serviço de produtos no API Gateway

--- GeekShopping.APIGateway/Program.cs
+++ GeekShopping.APIGateway/Program.cs
@@ -1,17 +1,28 @@
+using Microsoft.IdentityModel.Tokens;
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.AddAuthentication("Bearer")
+    .AddJwtBearer("Bearer", options =>
+    {
+        options.Authority = "http://localhost:4436/";
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateAudience = false
+        };
+        options.RequireHttpsMetadata = false;
+    });
+
+builder.Services.AddOcelot();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
+app.UseOcelot();
 
 app.Run();
